d2b17c6a6476d32e2e762f0fb4a015e7cb623f88
anthonypeters987 <anthonypeters987@gmail.com>
2022-07-27 13:12:39 -0400

    set up main

--- HW5-peters.java
+++ HW5-peters.java
@@ -16,3 +16,14 @@
  *
  * Driver class used to create a Register object then run it, after it finishes running it will exit the program
  */
+
+public static void main(String[] args) {
+        // Create register object instance
+        Register register = new Register();
+
+        // Run register object
+        register.run();
+
+        // Finish
+        System.exit(0);
+}
