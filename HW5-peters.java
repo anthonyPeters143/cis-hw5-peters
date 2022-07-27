319ffa778a7be7f1e870e7d41138559cd3bb4c01
anthonyPeters143 <108294207+anthonyPeters143@users.noreply.github.com>
2022-07-27 12:56:23 -0400

    Update HW5-peters.java

--- HW5-peters.java
+++ HW5-peters.java
@@ -10,3 +10,24 @@
 // !!!!!!!!!!!!NEED TO FILLOUT BEOFRE TURN IN
 
 // Driver class for HW5
+
+/**
+ * @author Anthony Peters
+ *
+ * Driver class used to create a Register object then run it, after it finishes running it will exit the program
+ */
+
+public class HW5_peters {
+
+    public static void main(String[] args) {
+        // Create register object instance
+        Register register = new Register();
+
+        // Run register object
+        register.run();
+
+        // Finish
+        System.exit(0);
+    }
+
+}
