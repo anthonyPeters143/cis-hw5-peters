db9034a86b337a61ef810756828a91fe6f4fb550
anthonyPeters143 <108294207+anthonyPeters143@users.noreply.github.com>
2022-07-27 12:47:05 -0400

    Create HW5-peters

---
+++ HW5-peters.java
@@ -0,0 +1,12 @@
+// Conformance to the OO Design: xx %
+// Support of item change: full/partial/none
+// Support of random access file: yes/no
+// Javadoc conformed comments on the classes, methods, and attributes: full/partial/no
+// Handling wrong input and invalid input: full/partial/no
+// Program does not crash with exceptions: crashes/does not crash
+// Correct handling of payment and taxes: yes/partial/no
+// Overall layout of GUI and ease of use: almost perfect/good enough/not good but works
+
+// !!!!!!!!!!!!NEED TO FILLOUT BEOFRE TURN IN
+
+// Driver class for HW5
